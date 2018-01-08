d1420d328e113d36ad0a5118f22c6ff6562b999d
Robert Zimmerman <robert.zimmerman@sunriseid.com>
2018-01-08 10:47:01 -0800

    Initial Load

---
+++ scripts/myScripts.js
@@ -0,0 +1,12 @@
+function myFunction() {
+   document.getElementById("demo2").innerHTML = "Paragraph changed.";
+}
+
+
+function myCar() {
+    var person = "John Doe"; 
+    var carName = "Volvo";
+    var price;
+    document.getElementById("demoCarName").innerHTML = carName; 
+    document.getElementById("demoCarPrice").innerHTML = price; 
+}
